55411d4d8bcdef3d69a14abcc3a90f5957d9d9a7
Eunji Song <eunji.l.song@gmail.com>
2018-08-01 17:19:36 -0400

    routers done

--- server/api/users.js
+++ server/api/users.js
@@ -82,7 +82,8 @@ router.put('/:id/restoreone', (req, res, next) => {
 router.put('/:id/restoreall', (req, res, next) => {
   return User.findById(req.params.id)
     .then( user => {
-              User.update({'posts': [...user.posts, user.trash]}, {where: {id: req.params.id}, returning: true})
+      const newposts = user.posts.concat(user.trash);
+              User.update({'posts': newposts}, {where: {id: req.params.id}, returning: true})
       return User.update({'trash': []}, {where: {id: req.params.id}, returning: true})
     })
     .then(([_, updated]) => res.status(201).json(updated[0]))
