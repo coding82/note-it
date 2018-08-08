e722ebf4ab1798e58cb0ada519cb6da16e889733
Eunji Song <eunji.l.song@gmail.com>
2018-08-08 18:56:44 -0400

    router update

--- server/api/users.js
+++ server/api/users.js
@@ -32,19 +32,22 @@ router.post('/', (req, res, next) => {
 
 router.put('/:id/newpost', (req, res, next) => {
   // req.body.content
+  console.log(req.body)
   return User.update({'posts': Sequelize.fn('array_append', Sequelize.col('posts'), req.body.content)}, { where: {id: req.params.id}, returning: true})
     .then(([_, updated]) => res.status(201).json(updated[0]))
     .catch(next)
 })
 
-////// DELETE ONE //////
+////// MOVE ONE TO TRASH //////
 //PUT - delete single post
 
 router.put('/:id/onetotrash', (req, res, next) => {
   // put input - req.body.id
+  const id = +Object.keys(req.body)
+
   return User.findById(req.params.id)
     .then( user => {
-      let outItem = user.posts.splice( +req.body.id, 1)
+      let outItem = user.posts.splice( +id, 1)
               User.update(
                 {'trash': Sequelize.fn('array_append', Sequelize.col('trash'), outItem[0])},
                 {where: {id: req.params.id},
@@ -58,12 +61,32 @@ router.put('/:id/onetotrash', (req, res, next) => {
     .catch(next)
 })
 
+
+
+router.put('/:id/deleteoneforever', (req, res, next) => {
+  // put input - req.body.id
+  const id = +Object.keys(req.body)
+
+  return User.findById(req.params.id)
+    .then( user => {
+      user.posts.splice( +id, 1)
+       return User.update(
+                {'trash': user.posts},
+                {where: {id: req.params.id},
+                returning: true})
+        .then(([_, updated]) => res.status(201).json(updated[0]))
+    })
+    .catch(next)
+})
+
 /////// RESTORE ONE ///////
 
 router.put('/:id/restoreone', (req, res, next) => {
+  const id = +Object.keys(req.body)
+
   return User.findById(req.params.id)
     .then( user => {
-      let outItem = user.trash.splice(+req.body.id, 1)
+      let outItem = user.trash.splice(id, 1)
               User.update(
                 {'posts': Sequelize.fn('array_append', Sequelize.col('posts'), outItem[0])},
                 {where: {id: req.params.id},
@@ -89,16 +112,28 @@ router.put('/:id/restoreall', (req, res, next) => {
     .then(([_, updated]) => res.status(201).json(updated[0]))
 })
 
+/////// MOVE ALL TO TRASH //////
+
+router.put('/:id/movealltotrash', (req, res, next) => {
+  return User.findById(req.params.id)
+    .then( user => {
+      const newtrash = user.trash.concat(user.posts);
+              User.update({'trash': newtrash}, {where: {id: req.params.id}, returning: true})
+      return User.update({'posts': []}, {where: {id: req.params.id}, returning: true})
+    })
+    .then(([_, updated]) => res.status(201).json(updated[0]))
+})
 
 
-////// DELETE ALL //////
+
+////// DELETE ALL THE POSTS FROM THE POST BIN //////
 // PUT - Empty the posts array
 
-router.put('/:id/emptyposts', (req, res, next) => {
-  return User.update({'posts': [] }, {where: {id: req.params.id}, returning: true})
-    .then(([_, updated]) => res.status(201).json(updated[0]))
-    .catch(next)
-})
+// router.put('/:id/emptyposts', (req, res, next) => {
+//   return User.update({'posts': [] }, {where: {id: req.params.id}, returning: true})
+//     .then(([_, updated]) => res.status(201).json(updated[0]))
+//     .catch(next)
+// })
 
 ////// DELETE ALL //////
 // PUT - Empty the trash array
