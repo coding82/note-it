14e8fa7d1151fc7f9ea8a2ae6677da7cd9d33433
Eunji Song <eunji.l.song@gmail.com>
2018-08-08 13:10:38 -0400

    auth fixed

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,41 +1,12 @@
 const router = require('express').Router()
-const User = require('../db/models/user')
 module.exports = router
 
-router.post('/login', (req, res, next) => {
-  User.findOne({where: {email: req.body.email}})
-    .then(user => {
-      if (!user) {
-        res.status(401).send('User not found')
-      } else if (!user.correctPassword(req.body.password)) {
-        res.status(401).send('Incorrect password')
-      } else {
-        req.login(user, err => (err ? next(err) : res.json(user)))
-      }
-    })
-    .catch(next)
+router.use('/auth', require('./auth'))
+
+router.use((req, res, next) => {
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
 })
 
-router.post('/signup', (req, res, next) => {
-  User.create(req.body)
-    .then(user => {
-      req.login(user, err => (err ? next(err) : res.json(user)))
-    })
-    .catch(err => {
-      if (err.name === 'SequelizeUniqueConstraintError') {
-        res.status(401).send('User already exists')
-      } else {
-        next(err)
-      }
-    })
-})
 
-router.post('/logout', (req, res) => {
-  req.logout()
-  req.session.destroy()
-  res.redirect('/')
-})
-
-router.get('/me', (req, res) => {
-  res.json(req.user)
-})
